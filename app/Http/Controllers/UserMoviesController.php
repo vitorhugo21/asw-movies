27aeb2b07dac16479be18b38f215461b6bdd33d2
Vítor Tavares <vitortavares21@ua.pt>
2019-12-20 22:40:08 +0000

    dynamic profile watchLater | Favorites | Viewed

--- app/Http/Controllers/UserMoviesController.php
+++ app/Http/Controllers/UserMoviesController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Classes\TheMovieDBClass;
+
 
 class UserMoviesController extends Controller
 {
-     /**
+
+    private $movie_class;
+
+    /**
      * Create a new controller instance.
      *
      * @return void
@@ -14,19 +20,37 @@ class UserMoviesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->movie_class = new TheMovieDBClass();
     }
 
-     /**
+    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        $favMovies = \Auth::user()->movies()->where('favorite',1)->get();
-        $watchLaterMovies = \Auth::user()->movies()->where('watch_later',1)->get();
-        $viewedMovies = \Auth::user()->movies()->where('viewed',1)->get();
+        $user = Auth::user();
 
-        return view('user-profile', ['user' => \Auth::user(), 'favorites' => $favMovies, 'watchLater' => $watchLaterMovies, 'viewed' => $viewedMovies]);
+        $movieList['favorites'] = $this->getMovieList($user->movies()->where('favorite', 1)->get());
+        $movieList['watchLater'] = $this->getMovieList($user->movies()->where('watch_later', 1)->get());
+        $movieList['viewed'] = $this->getMovieList($user->movies()->where('viewed', 1)->get());
+        //return $movieList;
+        return view('user-profile', [
+            'movies' => $movieList
+        ]);
+    }
+
+    private function getMovieList($movieList)
+    {
+        if (count($movieList) !== 0) {
+            $movieArray = [];
+            foreach ($movieList as $movie) {
+                array_push($movieArray, $this->movie_class->getMovie($movie['movie_id']));
+            }
+            return $movieArray;
+        } else {
+            return 0;
+        }
     }
 }
--- resources/views/user-profile.blade.php
+++ resources/views/user-profile.blade.php
@@ -1,94 +1,100 @@
 @extends('layouts.layout')
 
-@section('title', 'ASW-MOVIES - '.$user['username'])
+@section('title', 'ASW-MOVIES - '. Auth::user()->name)
 @section('more_styles')
 <link rel="stylesheet" href="{{ asset('css/modal.css') }}">
 <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
 @endsection
 
 @section('content')
-<div class = "container">
-<div class="row">
+<div class="container">
+    <div class="row">
 
 
-    <div class="col d-flex justify-content-center">
-        <img src="{{ asset('img/default-avatar.png') }}" alt="" srcset="">
-    </div>
+        <div class="col d-flex justify-content-center">
+            <img src="{{ asset('img/default-avatar.png') }}" alt="" srcset="">
+        </div>
 
-    <div class="col">
+        <div class="col">
 
-        <div class="card">
+            <div class="card">
 
-            <div class="card-body">
-                <h4>Name: {{$user['name']}}</h4>
-                <h4>Username: {{$user['username']}}</h4>
-                <h4>Email: {{$user['email']}}</h4>
-                <p><a href="">Change my email</a></p>
-                <h4>Password: *****</h4>
-                <p><a href="">Change my password</a></p>
+                <div class="card-body">
+                    <h4>Name: {{Auth::user()->name}}</h4>
+                    <h4>Username: {{Auth::user()->username}}</h4>
+                    <h4>Email: {{Auth::user()->email}}</h4>
+                    <p><a href="">Change my email</a></p>
+                    <h4>Password: *****</h4>
+                    <p><a href="">Change my password</a></p>
+                </div>
+
             </div>
 
         </div>
 
     </div>
-
-</div>
-
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <h4>Favorite</h4>
-                <div class="container-fluid py-2">
-                    <div class="d-flex flex-row flex-nowrap overflow-auto">
-                        @foreach ($favorites as $favorite)
-                        <div class="pmcard card card-body">
-                            <span> {{$favorite['movie_id']}} </span>
+    @if ($movies['favorites'] !== 0)
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h4>Favorite</h4>
+                    <div class="container-fluid py-2">
+                        <div class="d-flex flex-row flex-nowrap overflow-auto">
+                            @foreach ($movies['favorites'] as $favorite)
+                            <div class="pmcard card card-body">
+                                <span> {{$favorite['original_title']}} </span>
+                                <img class="d-block w-100" src="https://image.tmdb.org/t/p/original{{$favorite['backdrop_path']}}" alt="" srcset="">
+                            </div>
+                            @endforeach
                         </div>
-                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <h4>Watch later</h4>
-                <div class="container-fluid py-2">
-                    <div class="d-flex flex-row flex-nowrap overflow-auto">
-                        @foreach ($watchLater as $watch)
-                        <div class="pmcard card card-body">
-                            <span> {{$watch['movie_id']}} </span>
+    @endif
+    @if ($movies['watchLater'] !== 0)
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h4>Watch later</h4>
+                    <div class="container-fluid py-2">
+                        <div class="d-flex flex-row flex-nowrap overflow-auto">
+                            @foreach ($movies['watchLater'] as $watch)
+                            <div class="pmcard card card-body">
+                                <span> {{$watch['original_title']}} </span>
+                                <img class="d-block w-100" src="https://image.tmdb.org/t/p/original{{$watch['backdrop_path']}}" alt="" srcset="">
+                            </div>
+                            @endforeach
                         </div>
-                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <h4>Viewed</h4>
-                <div class="container-fluid py-2">
-                    <div class="d-flex flex-row flex-nowrap overflow-auto">
-                        @foreach ($viewed as $watched)
-                        <div class="pmcard card card-body">
-                            <span> {{$watched['movie_id']}} </span>
+    @endif
+    @if ($movies['viewed'] !== 0)
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h4>Viewed</h4>
+                    <div class="container-fluid py-2">
+                        <div class="d-flex flex-row flex-nowrap overflow-auto">
+                            @foreach ($movies['viewed'] as $watched)
+                            <div class="pmcard card card-body">
+                                <span> {{$watched['original_title']}} </span>
+                                <img class="d-block w-100" src="https://image.tmdb.org/t/p/original{{$watched['backdrop_path']}}" alt="" srcset="">
+                            </div>
+                            @endforeach
                         </div>
-                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    @endif
 </div>
 @endsection
