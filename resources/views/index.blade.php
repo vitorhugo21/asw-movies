3a6ce24fa6e219984980ac55452c67ea34b0eb9c
Vítor Tavares <vitortavares21@ua.pt>
2020-01-05 23:41:43 +0000

    full screen image without scroll

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.layout')
+
 @section('title', config('app.name'))
- @section('content')
-   
-    <div class="content mw-100 mh-100">
+
+@section('content')
+<div class="container-fluid px-0">
+    <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
+
         <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
             <ol class="carousel-indicators">
                 @for ($index = 0; $index < count($movies); $index++) <li data-target="#carouselExampleIndicators" data-slide-to="{{ $index }}" class="@if($index == 0) active @endif">
@@ -12,26 +15,27 @@
             <div class="carousel-inner">
                 @foreach ($movies as $movie)
                 <div class="carousel-item @if($loop->first) active @endif">
-                    <img class="d-block w-100" src="https://image.tmdb.org/t/p/original{{$movie['backdrop_path']}}" alt="" srcset="">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>{{$movie['original_title']}}</h5>
+                    <img src="https://image.tmdb.org/t/p/original{{$movie['backdrop_path']}}" class="vw-100 vh-100" alt="{{$movie['original_title']}}" srcset="">
+                    <div class="carousel-caption">
+                        <h3 class="font-weight-bold text-uppercase">{{$movie['original_title']}}</h3>
                         <p>{{$movie['overview']}}</p>
-                       
                         <button type="submit" class="btn btn-primary" onclick="location.href='{{ route('movie', $movie['id']) }}';"> <a style="color:white">See details</a> </button>
-
                     </div>
                 </div>
                 @endforeach
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
+
+
+
         </div>
     </div>
-
- @endsection
+    @endsection
