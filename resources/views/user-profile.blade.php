8e973b400f35214ef1ab076be679e6f90cd85c7b
Irene <irenemteixeira@ua.pt>
2020-01-05 17:06:32 +0000

    wip styles movies user profile

--- resources/views/user-profile.blade.php
+++ resources/views/user-profile.blade.php
@@ -72,82 +72,152 @@
         </div>
 
     </div>
-    @if ($movies['favorites'] !== 0)
-    <div class="row">
-        <div class="col">
+    @if (count($movies['favorites']) !== 0)
+    <p>
+        <button class="btn btn-primary w-100 p-2 font-weight-bold" type="button" data-toggle="collapse"
+            data-target="#favorites_movies" aria-expanded="false" aria-controls="collapseExample">
+            FAVORITES
+        </button>
+    </p>
+    <div class="collapse" id="favorites_movies">
+        <div id="multi-item-example-favorite" class="carousel slide carousel-multi-item" data-ride="carousel">
+            <!--Controls-->
+            <div class="controls-top d-flex justify-content-center">
+                <a class="btn-floating h2 px-2" href="#multi-item-example-favorite" data-slide="prev"><i
+                        class="fas fa-chevron-circle-left"></i></a>
+                <a class="btn-floating h2 px-2" href="#multi-item-example-favorite" data-slide="next"><i
+                        class="fa fa-chevron-circle-right"></i></a>
+            </div>
+            <!--/.Controls-->
+            @php
+            $totalFavorites = count($movies['favorites']);
+            $divideFavoritesPerFive = ceil($totalFavorites / 5);
+            @endphp
+            <!--Indicators-->
+            <ol class="carousel-indicators">
+                @for ($index = 0; $index < $divideFavoritesPerFive; $index++) <li
+                    data-target="#multi-item-example-favorite" data-slide-to="{{ $index }}"
+                    class="@if($index == 0) active @endif">
+                    </li>
+                    @endfor
+            </ol>
+            <!--/.Indicators-->
+            <div class="carousel-inner">
+                @for ($index = 0; $index < round(($totalFavorites/10))*10; $index+=5) <div
+                    class="carousel-item @if($index === 0) active @endif">
+                    <div class="row">
+                        @foreach (array_slice($movies['favorites'], $index, 5) as $favorite)
+                        <pre>{{$favorite}}</pre>
+                        <div class="col">
 
-            <div class="card">
-                <div class="card-body">
-                    <h4>Favorite</h4>
-                    <div class="container-fluid py-2">
-                        <div class="d-flex flex-row flex-nowrap overflow-auto">
-                            @foreach ($movies['favorites'] as $favorite)
-                            <a href='{{ route('movie', $favorite['id']) }}'>
-                                <div class="pmcard card card-body">
-                                    <span> {{$favorite['original_title']}} </span>
-                                    <img class="d-block w-100"
-                                        src="https://image.tmdb.org/t/p/original{{$favorite['backdrop_path']}}" alt=""
-                                        srcset="">
+                            <div class="container">
+                                <div class="d-flex flex-column align-items-center">
+                                    <a href='{{ route('movie', $favorite['id']) }}'>
+                                        <div class="pmcard card card-body">
+                                            <span> {{$favorite['original_title']}} </span>
+                                            <img class="d-block w-100"
+                                                src="https://image.tmdb.org/t/p/original{{$favorite['backdrop_path']}}"
+                                                alt="" srcset="">
+                                        </div>
+                                    </a>
+                                    <br>
+                                    <div class="">
+                                        <a href='{{ route('movie', $favorite['id']) }}'
+                                            class="text-decoration-none text-reset">
+                                            <h5 class="movieFavorite">{{$favorite['name']}}</h5>
+                                        </a>
+                                    </div>
                                 </div>
-                            </a>
-                            @endforeach
+                            </div>
                         </div>
+                        @endforeach
+                    </div>
+                    <br>
+            </div>
+            @endfor
+        </div>
+    </div>
+</div>
+@endif
+
+
+
+@if ($movies['favorites'] !== 0)
+<div class="row">
+    <div class="col">
+
+        <div class="card">
+            <div class="card-body">
+                <h4>Favorite</h4>
+                <div class="container-fluid py-2">
+                    <div class="d-flex flex-row flex-nowrap overflow-auto">
+                        @foreach ($movies['favorites'] as $favorite)
+                        <a href='{{ route('movie', $favorite['id']) }}'>
+                            <div class="pmcard card card-body">
+                                <span> {{$favorite['original_title']}} </span>
+                                <img class="d-block w-100"
+                                    src="https://image.tmdb.org/t/p/original{{$favorite['backdrop_path']}}" alt=""
+                                    srcset="">
+                            </div>
+                        </a>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endif
-    @if ($movies['watchLater'] !== 0)
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h4>Watch later</h4>
-                    <div class="container-fluid py-2">
-                        <div class="d-flex flex-row flex-nowrap overflow-auto">
-                            @foreach ($movies['watchLater'] as $watch)
-                            <a href='{{ route('movie', $watch['id']) }}'>
-                                <div class="pmcard card card-body">
-                                    <span> {{$watch['original_title']}} </span>
-                                    <img class="d-block w-100"
-                                        src="https://image.tmdb.org/t/p/original{{$watch['backdrop_path']}}" alt=""
-                                        srcset="">
-                                </div>
-                            </a>
-                            @endforeach
-                        </div>
+</div>
+@endif
+@if ($movies['watchLater'] !== 0)
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h4>Watch later</h4>
+                <div class="container-fluid py-2">
+                    <div class="d-flex flex-row flex-nowrap overflow-auto">
+                        @foreach ($movies['watchLater'] as $watch)
+                        <a href='{{ route('movie', $watch['id']) }}'>
+                            <div class="pmcard card card-body">
+                                <span> {{$watch['original_title']}} </span>
+                                <img class="d-block w-100"
+                                    src="https://image.tmdb.org/t/p/original{{$watch['backdrop_path']}}" alt=""
+                                    srcset="">
+                            </div>
+                        </a>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endif
-    @if ($movies['viewed'] !== 0)
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h4>Viewed</h4>
-                    <div class="container-fluid py-2">
-                        <div class="d-flex flex-row flex-nowrap overflow-auto">
-                            @foreach ($movies['viewed'] as $watched)
-                            <a href='{{ route('movie', $watched['id']) }}'>
-                                <div class="pmcard card card-body">
-                                    <span> {{$watched['original_title']}} </span>
-                                    <img class="d-block w-100"
-                                        src="https://image.tmdb.org/t/p/original{{$watched['backdrop_path']}}" alt=""
-                                        srcset="">
-                                </div>
-                            </a>
-                            @endforeach
-                        </div>
+</div>
+@endif
+@if ($movies['viewed'] !== 0)
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h4>Viewed</h4>
+                <div class="container-fluid py-2">
+                    <div class="d-flex flex-row flex-nowrap overflow-auto">
+                        @foreach ($movies['viewed'] as $watched)
+                        <a href='{{ route('movie', $watched['id']) }}'>
+                            <div class="pmcard card card-body">
+                                <span> {{$watched['original_title']}} </span>
+                                <img class="d-block w-100"
+                                    src="https://image.tmdb.org/t/p/original{{$watched['backdrop_path']}}" alt=""
+                                    srcset="">
+                            </div>
+                        </a>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endif
+</div>
+@endif
 </div>
 
 @endsection
